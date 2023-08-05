99a60c615c6c7b9e9480bfb8e18cf140402f6a4e
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2023-08-05 22:01:11 +0530

    #comment custom percentage field added and switch condition for round off added

--- app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.tipcalculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,12 +29,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,20 +68,45 @@ fun TipCalculator(modifier: Modifier = Modifier){
        mutableStateOf("0")
     }
 
+    var percentage by remember {
+        mutableStateOf("0")
+    }
+
+    var isChecked by remember {
+        mutableStateOf(false)
+    }
+
     Column(verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Text(text = "Calculate Tip", modifier = Modifier
+        val modifier1 = Modifier
             .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp), textAlign = TextAlign.Left)
+            .padding(start = 20.dp, end = 20.dp)
+
+        Text(text = "Calculate Tip", modifier = modifier1, textAlign = TextAlign.Left)
+
         Spacer(modifier = Modifier.padding(10.dp))
 
-        EditTextLayout(value = amount, onValueChanged = {amount = it})
+        EditTextLayout(value = amount, onValueChanged = {amount = it},
+            R.string.bill_amt,
+            androidx.core.R.drawable.notification_bg,modifier1)
 
         Spacer(modifier = Modifier.padding(10.dp))
 
-        Text(text = "Tip Amount: $"+ getCalculatedTip(amount),
+        EditTextLayout(value = percentage, onValueChanged = {percentage = it},
+            R.string.tip_percentage,
+            androidx.core.R.drawable.notification_action_background, modifier1)
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        RoundValue(value = isChecked, onChecked = {isChecked = it}, modifier1)
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Text(text = stringResource(id = R.string.tip_amt)+ getCalculatedTip(amount, percentage, isChecked),
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
             maxLines = 1,
@@ -90,18 +123,29 @@ fun TipCalculator(modifier: Modifier = Modifier){
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditTextLayout(value: String,onValueChanged: (String)->Unit){
-    TextField(value = value, onValueChange = onValueChanged, modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 20.dp, end = 20.dp),
+fun EditTextLayout(value: String,onValueChanged: (String)->Unit,
+                   label: Int, leadingIcon: Int, modifier: Modifier){
+    TextField(value = value,
+        onValueChange = onValueChanged,
+        modifier = modifier,
         singleLine = true,
-        label = { Text(text = "Bill Amount")},
+        label = { Text(text = stringResource(id = label))},
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
 }
 
-fun getCalculatedTip(value: String): Float{
-    return if(value.isNotEmpty()) {
-        0.15f * value.toFloat()
+@Composable
+fun RoundValue(value: Boolean, onChecked: (Boolean)-> Unit, modifier: Modifier){
+    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(text = stringResource(id = R.string.round_up_tip), textAlign = TextAlign.Center)
+        Switch(checked = value, onCheckedChange = onChecked)
+    }
+}
+
+fun getCalculatedTip(value: String, percentage: String, roundOffEnabled: Boolean): Float{
+    return if(value.isNotEmpty() && percentage.isNotEmpty()) {
+        var x = percentage.toFloat() * value.toFloat() / 100f
+        if(roundOffEnabled) x = ceil(x.toDouble()).toFloat()
+        return x
     }else 0.0f
 }
 
